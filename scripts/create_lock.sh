70be493828ab7bd16c6bea876d338be7798a6df7
Artem Fediai <artem.fediai@nanomatch.com>
2024-05-17 15:47:41 +0200

    lock updated to work with updated qp

--- scripts/create_lock.sh
+++ scripts/create_lock.sh
@@ -1,3 +1,3 @@
-docker run -it --rm -v $(pwd):/tmp mambaorg/micromamba:1.3.1 \
+docker run -it --rm -v $(pwd):/tmp -u $(id -u):$(id -g)  mambaorg/micromamba:latest \
    /bin/bash -c "micromamba create --yes --name new_env --file /tmp/env.yml && \
                  micromamba env export --name new_env --explicit > env.lock"
