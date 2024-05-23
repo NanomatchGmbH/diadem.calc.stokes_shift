9740b7699e4b4ca55f70a31187d455b00c0a13fa
Artem Fediai <artem.fediai@nanomatch.com>
2024-05-23 10:08:01 +0200

    4 is the hards-coded number of cores; deposit_init.sh

--- diadem_image_template/entrypoint.sh
+++ diadem_image_template/entrypoint.sh
@@ -3,12 +3,22 @@
 # This file will start automatically in your docker run. You can assume the presence of a 
 # molecule.yml and a calculator.yml in the work directory.
 
-export OMP_NUM_THREADS=$(nproc)  # nproc returns the number of available cores
-# export NM_LICENSE_SERVER=IP < -- ?
+#ncpus=`./get_num_cpu.sh` # return number of physical cpus. for test only? DOES NOT WORK SO FAR
+
+ncpus=4
+
+export OMP_NUM_THREADS=${ncpus}
+
+export UC_PROCESSORS_PER_NODE=${ncpus}
+
+#echo ${ncpus}
 
 export NM_LICENSE_SERVER=123.123.123.123
 
 python /opt/get_mobility.py
+
+bash deposit_init.sh >> log.txt 2>&1
+
 
 # Make sure that after this script finishes a result.yml exists.
 # The workdir_bundle.tar.gz will also be staged out for debugging purposes, if you create it. 
