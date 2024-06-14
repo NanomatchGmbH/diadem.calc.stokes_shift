2cb6f4972597e9e8beb5e769af5ce9037165b400
Artem.Fediai <artem.fedyay@gmail.com>
2024-06-14 11:12:42 +0200

    test separated hole/electron mobility parser

--- tests/other_tests/test_result_lightforge.py
+++ tests/other_tests/test_result_lightforge.py
@@ -5,7 +5,24 @@ import numpy as np
 @pytest.fixture
 def local_result_template():
     return {
-        "mobility": {
+        "hole_mobility": {
+            "value": None,
+            "results": {
+                "fields": {
+                    "values": [],
+                    "units": "V/nm"
+                },
+                "mobilities": {
+                    "values": [],
+                    "units": "cm²/V·s"
+                },
+                "stderr": {
+                    "values": [],
+                    "units": "cm²/V·s"
+                }
+            }
+        },
+        "electron_mobility": {
             "value": None,
             "results": {
                 "fields": {
@@ -31,20 +48,23 @@ def test_lightforge(local_result_template):
 
 
     # Run the lightforge function
-    get_result_from.lightforge(local_result_template, str(mobilities_file), str(settings_file))
+    get_result_from.lightforge(local_result_template, str(mobilities_file), str(settings_file), hole_or_electron='hole')
+    get_result_from.lightforge(local_result_template, str(mobilities_file), str(settings_file), hole_or_electron='electron')
 
     # Print the result for debugging
     print(local_result_template)
 
     # Assert the expected values
-    assert local_result_template["mobility"]["results"]["fields"]["values"] == [0.2, 0.3, 0.4]
-    assert local_result_template["mobility"]["results"]["mobilities"]["values"] == pytest.approx([
-        1.186932741712129827e-02, 2.732111808085449511e-02, 3.904151254044361391e-02
-    ], rel=1e-6)
-    assert local_result_template["mobility"]["results"]["stderr"]["values"] == pytest.approx([
-        2.157156189860654893e-03/np.sqrt(10), 8.981448462374695338e-03/np.sqrt(10), 1.049719255916519572e-02 / np.sqrt(10)
-    ], rel=1e-6)
-    assert local_result_template["mobility"]["value"] is not None  # Check if zero-field mobility is set
+    for hole_or_electron in ['hole', 'electron']:
+        hole_or_electron_mobility = f'{hole_or_electron}_mobility'
+        assert local_result_template[hole_or_electron_mobility]["results"]["fields"]["values"] == [0.2, 0.3, 0.4]
+        assert local_result_template[hole_or_electron_mobility]["results"]["mobilities"]["values"] == pytest.approx([
+            1.186932741712129827e-02, 2.732111808085449511e-02, 3.904151254044361391e-02
+        ], rel=1e-6)
+        assert local_result_template[hole_or_electron_mobility]["results"]["stderr"]["values"] == pytest.approx([
+            2.157156189860654893e-03/np.sqrt(10), 8.981448462374695338e-03/np.sqrt(10), 1.049719255916519572e-02 / np.sqrt(10)
+        ], rel=1e-6)
+        assert local_result_template[hole_or_electron_mobility]["value"] is not None  # Check if zero-field mobility is set
 
 
 if __name__ == "__main__":
