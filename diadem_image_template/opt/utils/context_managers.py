5665a83bf6ae243d89eb644491dac7b86ab6d893
Artem Fediai <artem.fediai@nanomatch.com>
2024-10-21 11:28:14 +0200

    time track for every module added

--- diadem_image_template/opt/utils/context_managers.py
+++ diadem_image_template/opt/utils/context_managers.py
@@ -3,6 +3,7 @@
 import os
 import pathlib
 import structlog
+import time
 
 # Create a logger
 logger = structlog.get_logger()
@@ -23,15 +24,25 @@ class ChangeDirectory:
         self.new_path = pathlib.Path.cwd() / self.dir_name
         self.original_path = pathlib.Path.cwd()
         self.additional_parameter = additional_parameter
+        self.start_time = None  # Add a variable to store the start time
 
     def __enter__(self):
         logger.info(f"{self.dir_name} starts . . .")
         self.new_path.mkdir(exist_ok=True)
         os.chdir(self.new_path)
         logger.info(f"Changed directory to {self.new_path}")
+        self.start_time = time.time()  # Start timing here
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        elapsed_time = time.time() - self.start_time  # Calculate elapsed time
+
+        # time in hours minutes seconds: __h__m__s
+        hours, remainder = divmod(elapsed_time, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        readable_time = f"{int(hours):02}h{int(minutes):02}m{int(seconds):02}s"
+        logger.info(f"{self.dir_name} completed in {readable_time}.")
+
         os.chdir(self.original_path)
         logger.info(f". . . {self.dir_name} successful!")
         logger.info(f"Returned to original directory {self.original_path}")
